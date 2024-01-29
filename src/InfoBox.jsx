176158a87e7f14c81a780b8c6471084b7ae095d5
mallickarif <mallickarif454@gmail.com>
2024-01-29 16:42:32 +0530

    first commit

---
+++ src/InfoBox.jsx
@@ -0,0 +1,57 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import "./InfoBox.css";
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+
+export default function InfoBox ({ info }) {
+    const INIT_URL = 
+    "https://images.unsplash.com/photo-1572687413625-cb2c4d9c4d32?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGR1c3R5JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+     
+    const HOT_URL = 
+    "https://images.unsplash.com/photo-1504370805625-d32c54b16100?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const COLD_URL = 
+    "https://images.unsplash.com/photo-1564314968303-86c5df2b9a4c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Q29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+    const RAIN_URL = 
+    "https://images.unsplash.com/photo-1534088568595-a066f410bcda?q=80&w=1951&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+     
+    return (
+        <div className="InfoBox">
+            <div className="cardContainer">
+            <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image= {
+          info.humidity > 80
+           ? RAIN_URL 
+           : info.temp > 20 ? HOT_URL 
+           : COLD_URL
+          }
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+         {info.city} {
+            info.humidity > 80
+            ? <ThunderstormIcon />
+            : info.temp > 20 
+            ?  <WbSunnyIcon />
+            : <AcUnitIcon />
+         }
+        </Typography>
+        <Typography variant="body2" color="text.secondary" component={"span"}>
+        <p>Temperature = {info.temp}&deg;C</p>
+        <p>Humidity = {info.humidity}</p>
+        <p>Min Temp = {info.tempMin}&deg;C</p>
+        <p>Max Temp = {info.tempMax}&deg;C</p>
+        <p>The Weather cam be described as <i>{info.weather}</i> feels like {info.feelslike}&deg;C</p>
+        </Typography>
+      </CardContent>
+    </Card>
+      </div>
+      </div>
+    );
+}
